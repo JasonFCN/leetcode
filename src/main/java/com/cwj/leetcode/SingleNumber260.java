2059d62359a46b3ad4fa36cee475fcf81f5fc518
chenwujie <1980647842@qq.com>
2020-07-27 23:12:55 +0800

    singleNumber260

---
+++ src/main/java/com/cwj/leetcode/SingleNumber260.java
@@ -0,0 +1,28 @@
+package com.cwj.leetcode;
+
+import java.util.BitSet;
+
+public class SingleNumber260 {
+    public static void main(String[] args) {
+        int[] arr = singleNumber(new int[]{1,2,1,3,2,5});
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+    }
+
+    public static int[] singleNumber(int[] nums) {
+        int bitmask = 0;
+        for (int i = 0; i < nums.length; i++) {
+            bitmask ^= nums[i];
+        }
+        int diffBit = bitmask & (-bitmask);
+
+        int x = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if ((diffBit & nums[i]) != 0) {
+                x ^= nums[i];
+            }
+        }
+        return new int[] { x, bitmask ^ x };
+    }
+    
+}
